9dffde73d934cc764a2fd4274ab025d93188628d
vinceliuice <vinceliuice@hotmail.com>
2020-02-02 23:21:05 +0800

    Add dark version

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ AURORAE_DIR="$HOME/.local/share/aurorae/themes"
 SCHEMES_DIR="$HOME/.local/share/color-schemes"
 PLASMA_DIR="$HOME/.local/share/plasma/desktoptheme"
 LOOKFEEL_DIR="$HOME/.local/share/plasma/look-and-feel"
+LAYOUT_DIR="$HOME/.local/share/plasma/layout-templates"
 KVANTUM_DIR="$HOME/.config/Kvantum"
 
 THEME_NAME=ChromeOS
@@ -13,28 +14,20 @@ THEME_NAME=ChromeOS
 install() {
   local name=${1}
 
-  local AURORAE_THEME="${AURORAE_DIR}/${name}"
-  local PLASMA_THEME="${PLASMA_DIR}/${name}"
-  local LOOKFEEL_THEME="${LOOKFEEL_DIR}/com.github.vinceliuice.${name}"
-
-  [[ -d ${AURORAE_THEME} ]] && rm -rf ${AURORAE_THEME}
-  [[ -d ${PLASMA_THEME} ]] && rm -rf ${PLASMA_THEME}
-  [[ -d ${LOOKFEEL_THEME} ]] && rm -rf ${LOOKFEEL_THEME}
-  [[ -d ${KVANTUM_THEME}/${name} ]] && rm -rf ${KVANTUM_THEME}/${name}
-  [[ -d ${SCHEMES_DIR}/${name}.colors ]] && rm -rf ${SCHEMES_DIR}/${name}.colors
-
   mkdir -p                                                                           ${AURORAE_DIR}
   mkdir -p                                                                           ${SCHEMES_DIR}
   mkdir -p                                                                           ${PLASMA_DIR}
   mkdir -p                                                                           ${LOOKFEEL_DIR}
+  mkdir -p                                                                           ${LAYOUT_DIR}
   mkdir -p                                                                           ${KVANTUM_DIR}
 
-  cp -ur ${SRC_DIR}/aurorae/${name}                                                  ${AURORAE_DIR}
-  cp -ur ${SRC_DIR}/color-schemes/*.colors                                           ${SCHEMES_DIR}
-  cp -ur ${SRC_DIR}/Kvantum/*                                                        ${KVANTUM_DIR}
-  cp -ur ${SRC_DIR}/plasma/desktoptheme/${name}${ELSE_DARK}                          ${PLASMA_DIR}
-  cp -ur ${SRC_DIR}/color-schemes/${name}Dark.colors                                 ${PLASMA_DIR}/${name}/colors
-  cp -ur ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}              ${LOOKFEEL_DIR}
+  cp -r ${SRC_DIR}/aurorae/*                                                         ${AURORAE_DIR}
+  cp -r ${SRC_DIR}/color-schemes/*.colors                                            ${SCHEMES_DIR}
+  cp -r ${SRC_DIR}/Kvantum/*                                                         ${KVANTUM_DIR}
+  cp -r ${SRC_DIR}/plasma/desktoptheme/*                                             ${PLASMA_DIR}
+  cp -r ${SRC_DIR}/plasma/layout-templates/*                                         ${LAYOUT_DIR}
+  cp -r ${SRC_DIR}/color-schemes/${name}Dark.colors                                  ${PLASMA_DIR}/${name}/colors
+  cp -r ${SRC_DIR}/plasma/look-and-feel/*                                            ${LOOKFEEL_DIR}
 }
 
 echo "Installing '${THEME_NAME} kde themes'..."
